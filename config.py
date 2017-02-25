f8222c6f3bd72eded5439f6c7b1192807b41c544
Pieter De Clercq <pieterdeclercq@outlook.com>
2017-02-25 20:12:07 +0100

    Fixes

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 cf_email = "user@domain.com"  # CloudFlare email address
 cf_apikey = ""  # CloudFlare API key
-hosts = {"mydomain.com": ["server1", "server2", "mail"]}  # the hosts among with the A record names to update
+hosts = {"mydomain.com": ["server1.mydomain.com", "server2.mydomain.com"]}  # the hosts among with the A record names to update
