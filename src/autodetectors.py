5ec37c3a0e2a917cd2cd9a33ab46011b2644e502
Ruben Maes <ruben@janmaes.com>
2017-02-26 23:31:32 +0100

    Apparently IFCFG only likes cURL

--- src/autodetectors.py
+++ src/autodetectors.py
@@ -5,8 +5,8 @@ def ip() -> str:
     Gets the WAN IPv4 address.
     :return: WAN IPv4 address of this host
     """
-    req = requests.get("https://4.ifcfg.me/")
-    return req.text
+    req = requests.get("https://4.ifcfg.me/ip")
+    return req.text.strip()
 
 
 def ipv6() -> str:
@@ -14,8 +14,8 @@ def ipv6() -> str:
     Gets the WAN IPv6 address.
     :return: WAN IPv6 address of this host
     """
-    req = requests.get("https://6.ifcfg.me/")
-    return req.text
+    req = requests.get("https://6.ifcfg.me/ip")
+    return req.text.strip()
 
 
 recordValueDetectors = {"A": ip, "AAAA": ipv6}
